d5d2d182437b41024f83d68f96334c8ddf20e051
Jose John <jose.john@a-cti.com>
2016-02-12 12:05:10 +0530

    changes made by jose

--- Loopsandmathic/src/javacore/BrkCntue.java
+++ Loopsandmathic/src/javacore/BrkCntue.java
@@ -3,13 +3,8 @@ package javacore;
 public class BrkCntue {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		for(int i=0; i <= 1 ; i++)
-		
-		switch(i)
-		{
-		case(1):
-			System.out.println("break demo");
+	
+		System.out.println("break demo");
 		for (int k = 1; k <= 10; k++)
 	      {
 			
@@ -23,16 +18,18 @@ public class BrkCntue {
 
 	      System.out.println("Loop is over.");
 	
-	case (2):
+	
 		System.out.println("continue demo");
-	for (int k= 1; k <= 10; k++)
+		for (int k= 1; k <= 10; k++)
       {
          if (k % 2 == 0)
          {
             continue;    // skip next statement if i is even
          }
 
-         System.out.println(k + "");
-      }}
+         System.out.print(k + " ");
+      }
+ }
 
-}}
+}
+
